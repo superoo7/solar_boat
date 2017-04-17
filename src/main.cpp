41a927ac7cced8fda7c3d2bb91f8a0eb3c03aa0c
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-04-17 20:09:14 +0800

    Make the code compatible with Arduino Mega

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,40 @@
 #include "header.h"
 
-void serialPrint();
+void serialPrintResult();
 
 void setup()
 {
   turbidity.SETUP();
-  ultrasonic.SETUP();
+  // ultrasonic.SETUP();
   Serial.begin(9600);
-  temperature.SETUP();
+  // temperature.SETUP();
 
 }
 
 void loop()
 {
   turbidity.TURBIDITY();
-  ultrasonic.DISTANCE();
-  temperature.TEMPERATURE();
+  // ultrasonic.DISTANCE();
+  // temperature.TEMPERATURE();
   delay(10);
   // insert error handling
-  serialPrint();
+  serialPrintResult();
 
   delay(1000);
 
 }
 
-void serialPrint()
+void serialPrintResult()
 {
   Serial.print("Turbidity: ");
   Serial.println(turbidity.turbidityVoltageOutput);
-  Serial.print("Distance: ");
-  Serial.println(ultrasonic.distanceTravelled);
-  Serial.print("Celcius: ");
-  Serial.println(temperature.cel);
-  Serial.println("Farenheit: ");
-  Serial.println(temperature.farh);
-  Serial.println(" ");
+  // Serial.print("Distance: ");
+  // Serial.println(ultrasonic.distanceTravelled);
+  // Serial.print("Celcius: ");
+  // Serial.println(temperature.cel);
+  // Serial.println("Farenheit: ");
+  // Serial.println(temperature.farh);
+  // Serial.println(" ");
 
 
 }
