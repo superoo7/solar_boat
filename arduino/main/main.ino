df53991a99068fb2929a606e84ac944bb8e94b60
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-04-18 15:21:07 +0800

    updated testing for ultrasonic

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,26 +1,26 @@
 
 #include "Arduino.h"
-#include "sensors/turbidity/turbidity.h"
+//#include "sensors/turbidity/turbidity.h"
 #include "sensors/ultrasonic/ultrasonic.h"
-#include "sensors/temperature/temperature.h"
-#include "sensors/dissolvedO2/dissolvedO2.h"
+//#include "sensors/temperature/temperature.h"
+//#include "sensors/dissolvedO2/dissolvedO2.h"
 
 void serialPrintResult();
 
 void setup()
 {
-  turbidity.SETUP();
+  //turbidity.SETUP();
   ultrasonic.SETUP();
   Serial.begin(9600);
-  temperature.SETUP();
+  //temperature.SETUP();
 
 }
 
 void loop()
 {
-  turbidity.TURBIDITY();
+  //turbidity.TURBIDITY();
   ultrasonic.DISTANCE();
-  temperature.TEMPERATURE();
+  //temperature.TEMPERATURE();
   delay(10);
   // insert error handling
   serialPrintResult();
@@ -31,15 +31,15 @@ void loop()
 
 void serialPrintResult()
 {
-  Serial.print("Turbidity: ");
-  Serial.println(turbidity.turbidityVoltageOutput);
-   Serial.print("Distance: ");
-   Serial.println(ultrasonic.distanceTravelled);
-   Serial.print("Celcius: ");
-   Serial.println(temperature.cel);
-   Serial.println("Farenheit: ");
-   Serial.println(temperature.farh);
-   Serial.println(" ");
+  //Serial.print("Turbidity: ");
+  //Serial.println(turbidity.turbidityVoltageOutput);
+  Serial.print("Distance: ");
+  Serial.println(ultrasonic.distanceTravelled);
+//   Serial.print("Celcius: ");
+//   Serial.println(temperature.cel);
+//   Serial.println("Farenheit: ");
+//   Serial.println(temperature.farh);
+//   Serial.println(" ");
 
 
 }
