2c5d3e82b114cc8c2abed40ed8f3666b3ad04eb7
Johnson Lai <jlwhoo7@gmail.com>
2017-05-20 03:17:00 +0800

    Added ThingSpeak library

--- mega/src/header.h
+++ mega/src/header.h
@@ -3,7 +3,12 @@
 
 #include "Arduino.h"
 
+// internet
+#include "parts/thingspeakAPI/internet.h"
 
+// sensors
+#include "parts/temperature/temperature.h"      // LM35 temperature sensor
 
+// motor
 
 #endif
--- mega/src/main.cpp
+++ mega/src/main.cpp
@@ -3,10 +3,17 @@
 
 void setup()
 {
+  Serial.begin(9600);
+  wifishield.SETUP();
+  thingspeak.SETUP();
 
 }
 
 void loop()
 {
+  for(int cnt = 0; cnt <= 40; cnt++) {
+    thingspeak.UPDATE(1.1*cnt , 2.2*cnt , 3.3*cnt);
 
+    delay(20000); // ThingSpeak will only accept updates every 15 seconds.
+  }
 }
